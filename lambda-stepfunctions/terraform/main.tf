842bc165a2ac536efec5d64573f9ed0dc4be3ec1
qiross <lucasqreis@poli.ufrj.br>
2024-11-10 21:32:00 -0300

    stepfunctions works

---
+++ lambda-stepfunctions/terraform/main.tf
@@ -0,0 +1,121 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "qiross"
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "../lambda"
+  output_path = "../terraform/lambda.zip"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "process_data" {
+  function_name = "processDataLambda"
+  handler       = "lambda.lambda_handler"
+  runtime       = "python3.8"
+  role          = aws_iam_role.lambda_role.arn
+  filename      = data.archive_file.lambda_zip.output_path
+}
+
+resource "aws_api_gateway_rest_api" "example" {
+  name        = "ExampleAPI"
+  description = "API Gateway for Lambda integration"
+}
+
+resource "aws_api_gateway_resource" "lambda_resource" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  path_part   = "process"
+}
+
+resource "aws_api_gateway_method" "lambda_method" {
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  resource_id   = aws_api_gateway_resource.lambda_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.process_data.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.example.id
+  resource_id             = aws_api_gateway_resource.lambda_resource.id
+  http_method             = aws_api_gateway_method.lambda_method.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.process_data.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "example" {
+  depends_on = [aws_api_gateway_integration.lambda_integration]
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  stage_name  = "prod"
+}
+
+output "api_gateway_url" {
+  value = aws_api_gateway_deployment.example.invoke_url
+}
+
+resource "aws_lambda_function" "log_data" {
+  function_name = "logDataLambda"
+  handler       = "log_lambda.lambda_handler"
+  runtime       = "python3.8"
+  role          = aws_iam_role.lambda_role.arn
+  filename      = data.archive_file.log_lambda_zip.output_path
+}
+
+data "archive_file" "log_lambda_zip" {
+  type        = "zip"
+  source_dir  = "../loglambda"
+  output_path = "../terraform/log_lambda.zip"
+}
+
+resource "aws_sfn_state_machine" "data_processing_state_machine" {
+  name     = "DataProcessingStateMachine"
+  role_arn = aws_iam_role.lambda_role.arn
+  definition = jsonencode({
+    "Comment": "Data processing state machine",
+    "StartAt": "ProcessData",
+    "States": {
+      "ProcessData": {
+        "Type": "Task",
+        "Resource": aws_lambda_function.process_data.arn,
+        "Next": "LogData"
+      },
+      "LogData": {
+        "Type": "Task",
+        "Resource": aws_lambda_function.log_data.arn,
+        "End": true
+      }
+    }
+  })
+}
+
+output "step_function_arn" {
+  value = aws_sfn_state_machine.data_processing_state_machine.arn
+}
